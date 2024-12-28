6065e220f4251c61f4a2d274cbbdeac7c4e3a00e
Sooraj Parameswaran <srjpn123@gmail.com>
2024-12-28 17:27:06 +0530

    Changed to umd library

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'SkillsRadarComponent.js',
         library: 'SkillsRadarComponent',
-        libraryTarget: 'commonjs2',
+        libraryTarget: 'umd',
     },
     module: {
         rules: [
